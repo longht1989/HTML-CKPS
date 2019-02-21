33f82f175f07ae0e74f0e4acfe150bb962ed6484
longht <longht@vng.com.vn>
2019-02-21 10:20:13 +0700

    updated layout

--- build/Jscripts/custom.js
+++ build/Jscripts/custom.js
@@ -2,5 +2,11 @@
    Onload functions
    ==================================== */
 $(function() {
-    
+    var minHeight = window.outerHeight;
+    var currentHeight = $('body').outerHeight();
+    console.log(minHeight);
+    console.log(currentHeight);
+    if (currentHeight <= minHeight) {
+        $('.site-footer').addClass('pinned');
+    }
 });
